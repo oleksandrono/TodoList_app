047f03c86fd2629f60988fd703d4e9afa7201cf3
InterLink Intern <intern@intern-pc.interlink.office>
2019-07-09 13:05:42 +0300

    implementing delete task method

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,7 @@ let tasks = [];
 
 if(localStorage.getItem("tasks")!==null){
     tasks = JSON.parse(localStorage.getItem("tasks"));
-}
-else if(localStorage.getItem("tasks")===undefined){
-    tasks = [];
+    outTaskAfterLoad();
 }
 
 let addTaskForm = document.getElementById('addTaskForm');
@@ -15,71 +13,114 @@ addTaskForm.addEventListener('submit', (event)=>{
 });
 
 let submitAddTask = document.getElementById('addTaskSubmit');
-submitAddTask.addEventListener('click', (event)=>{
-    addTask();
-});
 
-//add task
+submitAddTask.onclick = ()=> addTask();
+
+
+function createTask(taskName, index){
+    let parrentElement = document.getElementById('tasksList');
+    let addTask = document.getElementById('addTask');
+
+    let createTaskBlock = document.createElement('div');
+    createTaskBlock.classList = 'task';
+    createTaskBlock.id = 'task'+index;
+    
+    let createCheckbox = document.createElement('input');
+    createCheckbox.type = 'checkbox';
+    createCheckbox.id = 'checkbox'+index;
+    createCheckbox.classList = 'checkbox';
+
+    let createP = document.createElement('p');
+    createP.id = 'taskName' + index;
+    createP.textContent = taskName;
+    
+    let createButtonsBlock = document.createElement('div');
+    createButtonsBlock.classList = 'buttons';
+
+    let createChangeButton = document.createElement('button');
+    createChangeButton.classList = 'button';
+    createChangeButton.id = 'changeTask' + index;
+    createChangeButton.textContent = 'Change';
+    createChangeButton.setAttribute('onclick', 'changeTask(event)');
+
+    let createDeleteButton = document.createElement('button');
+    createDeleteButton.classList = 'button';
+    createDeleteButton.id = 'deleteTask' + index;
+    createDeleteButton.textContent = 'Delete';
+    createDeleteButton.setAttribute('onclick', 'deleteTask(event)');
+
+
+    parrentElement.insertBefore(createTaskBlock, addTask);
+    createTaskBlock.appendChild(createCheckbox);
+    createTaskBlock.appendChild(createP);
+    createTaskBlock.appendChild(createButtonsBlock);
+    createButtonsBlock.appendChild(createChangeButton);
+    createButtonsBlock.appendChild(createDeleteButton);
+}
 
 function addTask(){
 
-    let taskName = document.getElementById('taskName');
-    let taskId = 0;
-    if(tasks.length>0){
-        tasks.forEach((element, index)=>{
-            taskId = index;
-        });  
-    }
-    else{
-        taskId = 0;
-    }
+    let task = {};
 
-    tasks.push(taskName);
-    
-    if(taskName.value===''){
+    let taskName = document.getElementById('taskName');
+
+    if(taskName.value.length<1){
         taskName.style.borderBottomColor = 'red';
         setTimeout(()=>{
             taskName.style.borderBottomColor = 'gray';  
         } ,1000);
-    }
+    }  
     else{
-        let parrentElement = document.getElementById('tasksList');
-
-        let createTaskBlock = document.createElement('div');
-        createTaskBlock.classList = 'task';
-        createTaskBlock.id = 'task'+taskId;
-        
-        let createCheckbox = document.createElement('input');
-        createCheckbox.type = 'checkbox';
-        createCheckbox.id = 'checkbox'+taskId;
-        createCheckbox.classList = 'checkbox';
-
-        let createP = document.createElement('p');
-        createP.id = 'taskName' + taskId;
-        createP.textContent = taskName.value;
-        
-        let createButtonsBlock = document.createElement('div');
-        createButtonsBlock.classList = 'buttons';
-
-        let createChangeButton = document.createElement('button');
-        createChangeButton.classList = 'button';
-        createChangeButton.id = 'changeTask' + taskId;
-        createChangeButton.textContent = 'Change';
-
-        let createDeleteButton = document.createElement('button');
-        createDeleteButton.classList = 'button';
-        createDeleteButton.id = 'deleteTask' + taskId;
-        createDeleteButton.textContent = 'Delete';
+        //give task id
+        let taskId = 0;
+        if(tasks.length===0){
+            taskId=0;
+        }
+        else if(tasks.length>0){
+            taskId=tasks.length;
+        }
 
 
-        parrentElement.appendChild(createTaskBlock);
-        createTaskBlock.appendChild(createCheckbox);
-        createTaskBlock.appendChild(createP);
-        createTaskBlock.appendChild(createButtonsBlock);
-        createButtonsBlock.appendChild(createChangeButton);
-        createButtonsBlock.appendChild(createDeleteButton);
+
+        task.taskId = taskId;
+        task.taskName = taskName.value;
+        tasks.push(task);
+
+        //add task
+        createTask(taskName.value, taskId);
+
+        taskName.value = '';
+
+        localStorage.setItem('tasks', JSON.stringify(tasks));
 
     }
+}
 
-    localStorage.setItem('tasks', tasks);
+function outTaskAfterLoad(){
+    if(tasks.length>=1){
+        for(let task in tasks){
+            createTask(tasks[task].taskName, tasks[task].taskId);
+        }
+    }
+}
+
+function deleteTask(event){ 
+    
+    let regex = /\d+/;
+
+    let deletedTaskIdNum = parseInt(event.target.id.match(regex));
+
+    for(let i = 0; i<tasks.length; i++){
+        if(tasks[i].taskId === deletedTaskIdNum){
+            tasks.splice(i, 1);
+            let deletedTask = document.getElementById('task'+deletedTaskIdNum);
+            deletedTask.remove();
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+        }
+    }
+
+}
+
+function changeTask(event){
+
 }
